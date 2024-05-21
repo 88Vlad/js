9f46fd04f27232e004d0e04126b900edd78f1b2b
Vlad Rv <rzhvlad@gmail.com>
2024-05-21 21:23:17 +0200

    исправил урок03

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-var num = 266219;
-var product = 1;
-var numString = num.toString();
-for (var i = 0; i < numString.length; i++) {
-    product *= parseInt(numString[i]);
+'use strict';
+
+const title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+const screenPrice = parseFloat(prompt("Сколько будет стоить данная работа?", "12000"));
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = parseFloat(prompt(`Сколько стоит ${service1}?`));
+const service2 = prompt("Какой еще дополнительный тип услуги нужен?");
+const servicePrice2 = parseFloat(prompt(`Сколько стоит ${service2}?`));
+
+// Calculating total price and discount
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const rollbackPercentage = 10;
+const maxDiscountThreshold = 30000;
+const minDiscountThreshold = 15000;
+let discountMessage;
+
+// Applying discount based on total price
+switch (true) {
+    case fullPrice >= maxDiscountThreshold:
+        discountMessage = "Даем скидку в 10%";
+        break;
+    case fullPrice >= minDiscountThreshold:
+        discountMessage = "Даем скидку в 5%";
+        break;
+    default:
+        discountMessage = "Скидка не предусмотрена";
+        break;
 }
 
-console.log("Произведение цифр:", product);
-var result = product * product * product;
-console.log("Результат возведения в степень 3:", result);
-var resultString = result.toString();
-var firstTwoDigits = parseInt(resultString.substring(0, 2));
-console.log("Первые две цифры полученного числа:", firstTwoDigits);
+// Calculating final price with rollback
+const servicePercentPrice = fullPrice - (fullPrice * (rollbackPercentage / 100));
+
+// Outputting results
+console.log(title);
+console.log(screens);
+console.log(service1);
+console.log(servicePrice1);
+console.log(service2);
+console.log(servicePrice2);
+console.log(discountMessage);
+console.log("Итоговая стоимость работы:", fullPrice, "руб.");
+console.log("Итоговая стоимость работы с учетом отката посреднику:", servicePercentPrice.toFixed(2), "руб.");
