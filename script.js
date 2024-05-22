84bca37d720309ef367dfd2e9b4808f451fd399f
Vlad Rv <rzhvlad@gmail.com>
2024-05-22 22:28:57 +0200

    переместил конст

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const title = prompt("Как называется ваш проект?");
 const screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
 const screenPrice = parseFloat(prompt("Сколько будет стоить данная работа?", "12000"));
@@ -8,15 +7,12 @@ const service1 = prompt("Какой дополнительный тип услу
 const servicePrice1 = parseFloat(prompt(`Сколько стоит ${service1}?`));
 const service2 = prompt("Какой еще дополнительный тип услуги нужен?");
 const servicePrice2 = parseFloat(prompt(`Сколько стоит ${service2}?`));
-
-// Calculating total price and discount
 const fullPrice = screenPrice + servicePrice1 + servicePrice2;
 const rollbackPercentage = 10;
 const maxDiscountThreshold = 30000;
 const minDiscountThreshold = 15000;
+const servicePercentPrice = fullPrice - (fullPrice * (rollbackPercentage / 100));
 let discountMessage;
-
-// Applying discount based on total price
 switch (true) {
     case fullPrice >= maxDiscountThreshold:
         discountMessage = "Даем скидку в 10%";
@@ -28,11 +24,6 @@ switch (true) {
         discountMessage = "Скидка не предусмотрена";
         break;
 }
-
-// Calculating final price with rollback
-const servicePercentPrice = fullPrice - (fullPrice * (rollbackPercentage / 100));
-
-// Outputting results
 console.log(title);
 console.log(screens);
 console.log(service1);
