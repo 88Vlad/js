000a5d9ccfcb5b306acf126ab99073f9252c0d96
Vlad <rzhvlad@gmail.com>
2024-07-19 23:07:53 +0200

    урок 10 первая часть

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 'use strict';
-
 // Получаем все книги и конвертируем NodeList в массив
 const booksContainer = document.querySelector('.books');
 const bookElements = Array.from(document.querySelectorAll('.book'));
-
 // Функция для сортировки книг по их названиям
 const sortBooks = () => {
     bookElements.sort((a, b) => {
@@ -11,23 +9,18 @@ const sortBooks = () => {
         const titleB = b.querySelector('h2 a').innerText;
         return titleA.localeCompare(titleB); // Сравнение строк
     });
-
     // Перемещаем книги в контейнер в отсортированном порядке
     bookElements.forEach(book => booksContainer.appendChild(book));
 };
 
-// Выполняем сортировку книг
 sortBooks();
 
 // Обновляем фон 
 document.querySelector('body').style.backgroundImage = 'url(./image/anotherbackground.jpg)';
-
 // Обновляем название одной из книг
 booksContainer.children[2].querySelector('h2 a').textContent = 'Книга 3. this и Прототипы Объектов';
-
 // Удаляем рекламный блок
 document.querySelector('.adv').remove();
-
 // Функция для сортировки глав в книге
 const sortChapter = (collection) => {
     // Функция для сортировки элементов списка
@@ -54,26 +47,21 @@ const sortChapter = (collection) => {
             appendices.push(el); // Добавляем в массив приложений
         }
     });
-
     // Сортируем и добавляем главы в конец списка
     sortElements(chapters).forEach(el => {
         collection.appendChild(el);
     });
-
     // Сортируем и добавляем приложения в конец списка
     sortElements(appendices).forEach(el => {
         collection.appendChild(el);
     });
 };
-
 // Сортируем главы и приложения для двух книг
 sortChapter(booksContainer.children[1].querySelector('ul'));
 sortChapter(booksContainer.children[4].querySelector('ul'));
-
 // Добавляем новую главу в последнюю книгу
 let newChapter = document.createElement('li');
 newChapter.innerText = 'Глава 8: За пределами ES6';
 booksContainer.children[5].querySelector('ul').appendChild(newChapter);
-
 // Сортируем главы и приложения для книги с новой главой
 sortChapter(booksContainer.children[5].querySelector('ul'));
